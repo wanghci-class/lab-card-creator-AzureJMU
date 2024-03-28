edcfcabac7a741adbc38d996b400a1f2ffea5637
Azure <dovelak@dukes.jmu.edu>
2024-03-28 14:48:12 -0400

    updated ish

---
+++ scripts/viewcards.js
@@ -0,0 +1,22 @@
+let cards = JSON.parse(localStorage.getItem("cards"));
+let template = document.querySelector("#card-template");
+let cardList = document.querySelector("#card-list");
+
+for (let i = 0; i < cards.length; i++)
+{
+    let card = cards[i];
+    console.log(card);
+
+    let cardView = template.content.cloneNode(true);
+
+    let titleText = cardView.querySelector(".title-text");
+    titleText.textContent = card.title;
+
+    // TODO rest of text spans
+    let deleteBtn = cardView.querySelector(".delete-btn")
+    deleteBtn.addEventListener("click", function {
+        console.log(i);
+    })
+
+    cardList.appendChild(cardView);
+}
