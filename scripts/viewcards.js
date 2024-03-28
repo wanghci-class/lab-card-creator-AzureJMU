4b7551b5f0481cd599dc01eea4e3f704d4323243
AzureJMU <127335749+AzureJMU@users.noreply.github.com>
2024-03-28 19:58:34 -0400

    fixed all the other stuff; got the messages to come through

--- scripts/viewcards.js
+++ scripts/viewcards.js
@@ -1,25 +1,36 @@
-let cards = JSON.parse(localStorage.getItem("cards"));
-let template = document.querySelector("#card-template");
-let cardList = document.querySelector("#card-list");
+let cards = JSON.parse(localStorage.getItem( "cards" ));
+let template = document.querySelector( "#card-template" );
+let cardList = document.querySelector( "#card-list" );
 
-for (let i = 0; i < cards.length; i++)
+for ( let i = 0; i < cards.length; i++ )
 {
     let card = cards[i];
     console.log(card);
 
-    let cardView = template.content.cloneNode(true);
+    let cardView = template.content.cloneNode( true );
 
-    let titleText = cardView.querySelector(".title-text");
+    let titleText = cardView.querySelector( ".title-text" );
     titleText.textContent = card.title;
 
-    let
-    let deleteBtn = cardView.querySelector(".delete-btn")
-    deleteBtn.addEventListener("click", function() {
-        console.log(i);
-        cards.splice(i, 1);
-        localStorage.setItem(JSON.stringify(cards));
+    let subtitleText = cardView.querySelector( ".subtitle-text" );
+    subtitleText.textContent = card.subtitle;
+
+    let toText = cardView.querySelector(".for-text");
+    toText.textContent = card.to;
+
+    let messageText = cardView.querySelector( ".message-text" );
+    messageText.textContent = card.message;
+
+    let fromText = cardView.querySelector( ".from-text" );
+    fromText.textContent = card.from;
+
+    let deleteBtn = cardView.querySelector( ".delete-btn" );
+    deleteBtn.addEventListener( "click", function() {
+        console.log( i );
+        cards.splice( i, 1 );
+        localStorage.setItem( JSON.stringify ( cards ) ) ;
         location.reload();
     })
 
-    cardList.appendChild(cardView);
+    cardList.appendChild( cardView );
 }
